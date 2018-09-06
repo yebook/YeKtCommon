361a4d9999a01e1a80b5ff6f8ee7d6e20a5884e7
kermitye <kermitye@163.com>
2018-09-06 17:38:50 +0800

    first commit

---
+++ app/src/main/java/com/kermitye/basepro/LaunchActivity.kt
@@ -0,0 +1,18 @@
+package com.kermitye.basepro
+
+import android.os.Bundle
+import com.kermitye.baselib.ui.BaseActivity
+
+/**
+ * Created by kermitye
+ * Date: 2018/9/6 17:00
+ * Desc:
+ */
+class LaunchActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launch)
+    }
+
+}
